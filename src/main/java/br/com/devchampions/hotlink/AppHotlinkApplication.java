db950866753fd6a5d5f15c0d95ac47ce077b9d0d
Dherkyan R. Silva <dherkyan@gmail.com>
2024-11-18 09:38:18 -0300

    Projeto Hotlink

---
+++ src/main/java/br/com/devchampions/hotlink/AppHotlinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.devchampions.hotlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHotlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHotlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devchampions/hotlink/AppHotlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devchampions.hotlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHotlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
