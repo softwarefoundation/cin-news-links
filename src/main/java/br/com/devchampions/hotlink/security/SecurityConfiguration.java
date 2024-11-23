6e0da4fbabf925686982b1ceb4e8e87d2b517250
Dherkyan R. Silva <dherkyan@gmail.com>
2024-11-23 10:29:26 -0300

    Substituir componente depreciado @EnableGlobalMethodSecurity por @EnableMethodSecurity

--- src/main/java/br/com/devchampions/hotlink/security/SecurityConfiguration.java
+++ src/main/java/br/com/devchampions/hotlink/security/SecurityConfiguration.java
@@ -3,7 +3,7 @@ package br.com.devchampions.hotlink.security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Slf4j
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
 
