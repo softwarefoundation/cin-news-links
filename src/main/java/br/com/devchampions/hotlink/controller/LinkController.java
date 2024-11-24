0fbe3187bd8dcd9a252889261e2bfba3691aad65
Dherkyan R. Silva <dherkyan@gmail.com>
2024-11-24 15:43:20 -0300

    Valiadar os campos de cadastro de links

--- src/main/java/br/com/devchampions/hotlink/controller/LinkController.java
+++ src/main/java/br/com/devchampions/hotlink/controller/LinkController.java
@@ -2,10 +2,14 @@ package br.com.devchampions.hotlink.controller;
 
 import br.com.devchampions.hotlink.entity.Link;
 import br.com.devchampions.hotlink.service.LinkService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +24,28 @@ public class LinkController {
     private LinkService linkService;
 
     @GetMapping("/cadastro")
-    public String viewCadastro() {
+    public String viewCadastro(Link link) {
         return "link/cadastro";
     }
 
     @PostMapping
-    public String salvar(Link link) {
+    public String salvar(@Valid Link link, BindingResult result, Model model) {
+
+        if (link.getTitulo().startsWith("http://") || link.getTitulo().startsWith("https://")) {
+            FieldError fieldError = new FieldError("link", "titulo", link.getTitulo(), false, null, null, "O campo título não deve iniciar com http ou https");
+            result.addError(fieldError);
+        }
+
+        if (!link.getEndereco().startsWith("http")) {
+            FieldError fieldError = new FieldError("link", "endereco", link.getEndereco(), false, null, null, "O campo endereço deve iniciar com http ou https");
+            result.addError(fieldError);
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("link", link);
+            return "link/cadastro";
+        }
+
         linkService.salvar(link);
         return "redirect:";
     }
--- src/main/java/br/com/devchampions/hotlink/entity/Link.java
+++ src/main/java/br/com/devchampions/hotlink/entity/Link.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -19,9 +21,11 @@ public class Link implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Campo obrigatório")
     @Column(name = "titulo", length = 200, nullable = false)
     private String titulo;
 
+    @NotBlank(message = "Campo obrigatório")
     @Column(name = "endereco", length = 1000, nullable = false, unique = true)
     private String endereco;
 
