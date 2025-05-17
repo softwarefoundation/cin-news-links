616b94aef599a647ac3d5133c6dbb03d23861842
Dherkyan R. Silva <dherkyan@gmail.com>
2025-05-17 07:34:34 -0300

    Autenticacao com keycloak concluida

---
+++ src/main/java/br/com/devchampions/hotlink/security/SecurityOauth2Configuration.java
@@ -0,0 +1,52 @@
+package br.com.devchampions.hotlink.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityOauth2Configuration {
+
+
+    @Value("${security.success-url}")
+    private String successUrl;
+
+    @Value("${security.logout-url}")
+    private String logoutUrl;
+
+    @Value("${security.redirect-uri}")
+    private String redirectUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .successHandler(new SimpleUrlAuthenticationSuccessHandler(successUrl))
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl(logoutUrl + "?redirect_uri=" + redirectUri)
+                );
+
+        return http.build();
+    }
+
+}
